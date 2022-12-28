e458ce44e099cab72b2d36909566ce6742ca6a27
MariaGoldina <maria_yastr@mail.ru>
2022-12-28 23:08:31 +0500

    Add solution of task 12.7

---
+++ main.py
@@ -0,0 +1,10 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+rates=per_cent.values()
+
+money=int(input("Введите сумму вклада: "))
+
+deposit_income=[x/100*money for x in rates]
+print(deposit_income)
+
+Max_income=max(deposit_income)
+print("Максимальная сумма, которую вы можете заработать — ", Max_income)
